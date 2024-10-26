99722933f11ff21103c0a3172db4463b838c8513
Alexander Kiselev <surgeonssad@gmail.com>
2024-10-26 17:06:48 +0300

    fix: Добавил удаление отзыва. [lab-5]

--- js/feedback.js
+++ js/feedback.js
@@ -100,5 +100,27 @@ document.addEventListener("DOMContentLoaded", function () {
 
         saveParams()
     });
+
+    document.getElementById("deleteTableButton").addEventListener("click", function () {
+        ratesContainer.innerHTML = '';
+        document.querySelectorAll('.criteria-checkbox').forEach(checkbox => {
+            checkbox.checked = false;
+        });
+        localStorage.removeItem("feedbackParams");
+
+        resultContainer.innerHTML = "";
+        let table = document.createElement("table");
+        table.classList.add("generated-table");
+
+        let headerRow = table.insertRow();
+        let selectedCheckboxes = Array.from(document.querySelectorAll('.criteria-checkbox:checked'));
+        selectedCheckboxes.forEach(c => {
+            let headerCell = document.createElement("th");
+            headerCell.textContent = c.value.charAt(0).toUpperCase() + c.value.slice(1);
+            headerRow.appendChild(headerCell);
+        });
+
+        resultContainer.appendChild(table);
+    });
 });
 
